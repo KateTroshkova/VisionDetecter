a9a0c8d61403a3ec9cbcc5c2e31759878d444395
Ekaterina Troshkova <e-troshkova@inbox.ru>
2017-09-20 21:15:43 +0700

    template detection

--- app/src/main/java/com/troshkova/portfolioprogect/visiondetector/FaceClassifier.java
+++ app/src/main/java/com/troshkova/portfolioprogect/visiondetector/FaceClassifier.java
@@ -4,11 +4,16 @@ import android.content.Context;
 
 import com.troshkova.portfolioprogect.visiondetector.exception.Exception;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -21,6 +26,7 @@ public class FaceClassifier {
     private Context context;
     private SmartCamera.OnCameraExceptionListener listener;
     private CascadeClassifier classifier;
+    private CascadeClassifier eyeClassifier;
 
     public FaceClassifier(Context context, SmartCamera.OnCameraExceptionListener listener){
         this.context=context;
@@ -28,6 +34,10 @@ public class FaceClassifier {
         String path=rewriteDataSource();
         if (path!=null){
             classifier = new CascadeClassifier(path);
+        }
+        String eyePath=rewriteEyeDataSource();
+        if (eyePath!=null){
+            eyeClassifier=new CascadeClassifier(eyePath);
         }
     }
 
@@ -39,6 +49,13 @@ public class FaceClassifier {
         MatOfRect faces = new MatOfRect();
         classifier.detectMultiScale(grayScaleImage, faces, 1.1, 2, 2, new Size(faceSize, faceSize), new Size());
         return faces.toArray();
+    }
+
+    public Rect[] getEyes(Mat grayScaleImage, Rect eyeArea){
+        MatOfRect eyes=new MatOfRect();
+        eyeClassifier.detectMultiScale(grayScaleImage.submat(eyeArea), eyes, 1.15, 2, Objdetect.CASCADE_FIND_BIGGEST_OBJECT
+                        | Objdetect.CASCADE_SCALE_IMAGE, new Size(30, 30), new Size());
+        return eyes.toArray();
     }
 
     private String rewriteDataSource(){
@@ -54,7 +71,33 @@ public class FaceClassifier {
             }
             input.close();
             output.close();
+
             return outputFile.getAbsolutePath();
+        }
+        catch(FileNotFoundException e){
+            listener.onCameraExceptionListener(Exception.EXCEPTION_NO_FILE);
+        }
+        catch(IOException e){
+            listener.onCameraExceptionListener(Exception.EXCEPTION_IO);
+        }
+        return null;
+    }
+
+    private String rewriteEyeDataSource(){
+        try {
+            InputStream einput = context.getResources().openRawResource(R.raw.haarcascade_lefteye_2splits);
+            File eyeDir = context.getDir("cascadeER", Context.MODE_PRIVATE);
+            File eyeFile = new File(eyeDir, "haarcascade_eye_right.xml");
+            FileOutputStream eoutput = new FileOutputStream(eyeFile);
+
+            byte[] ebuffer = new byte[4096];
+            int ebytesRead;
+            while ((ebytesRead = einput.read(ebuffer)) != -1) {
+                eoutput.write(ebuffer, 0, ebytesRead);
+            }
+            einput.close();
+            eoutput.close();
+            return eyeFile.getAbsolutePath();
         }
         catch(FileNotFoundException e){
             listener.onCameraExceptionListener(Exception.EXCEPTION_NO_FILE);
--- app/src/main/java/com/troshkova/portfolioprogect/visiondetector/SmartCamera.java
+++ app/src/main/java/com/troshkova/portfolioprogect/visiondetector/SmartCamera.java
@@ -12,8 +12,10 @@ import com.troshkova.portfolioprogect.visiondetector.exception.Exception;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
@@ -30,6 +32,12 @@ public class SmartCamera extends JavaCameraView implements CameraBridgeViewBase.
         void onCameraExceptionListener(Exception exception);
     }
 
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        disableView();
+    }
+
     public SmartCamera(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.parent=(Activity)getContext();
@@ -40,6 +48,7 @@ public class SmartCamera extends JavaCameraView implements CameraBridgeViewBase.
                 setCvCameraViewListener(this);
                 if (libReady()) {
                     faceClassifier=new FaceClassifier(context, listener);
+                    enableFpsMeter();
                     enableView();
                 } else {
                     listener.onCameraExceptionListener(Exception.EXCEPTION_LIB_NOT_READY);
@@ -62,12 +71,12 @@ public class SmartCamera extends JavaCameraView implements CameraBridgeViewBase.
 
     @Override
     public void onCameraViewStopped() {
-
+        grayScaleImage.release();
     }
 
     @Override
     public Mat onCameraFrame(Mat inputFrame) {
-        Imgproc.cvtColor(inputFrame, grayScaleImage, Imgproc.COLOR_RGBA2RGB);
+        Imgproc.cvtColor(inputFrame, grayScaleImage, Imgproc.COLOR_RGBA2GRAY);
         Rect[] faces=null;
         if (faceClassifier.isExist()) {
             faces=faceClassifier.getFaces(grayScaleImage, faceSize);
@@ -80,10 +89,45 @@ public class SmartCamera extends JavaCameraView implements CameraBridgeViewBase.
                 listener.onCameraExceptionListener(Exception.EXCEPTION_MANY_FACES);
             }
             for (Rect face : faces) {
-                Imgproc.rectangle(inputFrame, face.tl(), face.br(), new Scalar(0.0, 255.0, 0.0, 255.0), 3);
+                drawFace(inputFrame, face);
             }
         }
         return inputFrame;
+    }
+
+    private void drawFace(Mat inputFrame, Rect face){
+        Imgproc.rectangle(inputFrame, face.tl(), face.br(), new Scalar(0.0, 255.0, 0.0, 255.0), 3);
+        Rect right = new Rect(face.x + face.width / 16, (int) (face.y + (face.height / 4.5)),
+                (face.width - 2 * face.width / 16) / 2, (int) (face.height / 3.0));
+        Rect left = new Rect(face.x + face.width / 16 + (face.width - 2 * face.width / 16) / 2,
+                (int) (face.y + (face.height / 4.5)), (face.width - 2 * face.width / 16) / 2, (int) (face.height / 3.0));
+        Imgproc.rectangle(inputFrame, left.tl(), left.br(),
+                new Scalar(0, 255, 0, 255), 2);
+        Imgproc.rectangle(inputFrame, right.tl(), right.br(),
+                new Scalar(0, 255, 0, 255), 2);
+        drawTemplate(inputFrame, right);
+        drawTemplate(inputFrame, left);
+    }
+
+    private void drawTemplate(Mat inputMat, Rect area) {
+        Point template = new Point();
+        Rect[] eyesArray = faceClassifier.getEyes(grayScaleImage, area);
+
+        for (Rect eye:eyesArray) {
+            eye.x = area.x + eye.x;
+            eye.y = area.y + eye.y;
+            Rect eyeRectangle = new Rect((int) eye.tl().x, (int) (eye.tl().y + eye.height * 0.4), (int) eye.width, (int) (eye.height * 0.6));
+
+            Core.MinMaxLocResult mmG = Core.minMaxLoc(grayScaleImage.submat(eyeRectangle));
+
+            Imgproc.circle(inputMat.submat(eyeRectangle), mmG.minLoc, 2, new Scalar(255, 0, 0, 255), 2);
+
+            template.x = mmG.minLoc.x + eyeRectangle.x;
+            template.y = mmG.minLoc.y + eyeRectangle.y;
+
+            Rect eyeTemplate = new Rect((int) template.x - 24 / 2, (int) template.y - 24 / 2, 24, 24);
+            Imgproc.rectangle(inputMat, eyeTemplate.tl(), eyeTemplate.br(), new Scalar(0, 255, 0, 255), 2);
+        }
     }
 
     private boolean hasCamera(){
