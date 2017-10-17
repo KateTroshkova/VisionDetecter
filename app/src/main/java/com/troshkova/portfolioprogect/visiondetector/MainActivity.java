0d11a2d500c3594d57c431010b8720795382a5fb
Ekaterina Troshkova <e-troshkova@inbox.ru>
2017-10-17 23:48:21 +0700

    fix bugs

--- app/src/main/java/com/troshkova/portfolioprogect/visiondetector/MainActivity.java
+++ app/src/main/java/com/troshkova/portfolioprogect/visiondetector/MainActivity.java
@@ -1,23 +1,15 @@
 package com.troshkova.portfolioprogect.visiondetector;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.CheckBox;
 
-import com.troshkova.portfolioprogect.visiondetector.exception.Exception;
-
-import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 
-public class MainActivity extends AppCompatActivity implements SmartCamera.OnCameraExceptionListener, SmartCamera.OnDetectSightListener {
+public class MainActivity extends AppCompatActivity implements SmartCamera.OnCameraExceptionListener, SmartCamera.OnEyeDirectionListener {
 
     private SmartCamera camera;
 
@@ -26,12 +18,10 @@ public class MainActivity extends AppCompatActivity implements SmartCamera.OnCam
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         camera=(SmartCamera)findViewById(R.id.camera);
-        camera.setRendering(true);
-    }
-
-    @Override
-    public void onCameraExceptionListener(Exception exception) {
-        Log.e("LOG", exception.toString());
+        camera.setDebugMode(true);
+        camera.setOnCameraExceptionListener(this);
+        camera.setOnDetectSightListener(this);
+        camera.startCamera();
     }
 
     @Override
@@ -42,11 +32,22 @@ public class MainActivity extends AppCompatActivity implements SmartCamera.OnCam
 
     //отражение по горизонтали
     public void mirror(View view){
-        camera.mirror();
+        camera.setMirror(((CheckBox) view).isChecked());
     }
 
     //поворот на -90 градусов
     public void rotateM90(View view){
-        camera.addRotation();
+        camera.setRotation(Rotation.ROTATION_270);
+    }
+
+    public void stop(View view){
+        camera.stopCamera();
+        //единственный способ ее остановить без ущерба для родительского класса-сделать невидимой
+        camera.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onCameraExceptionListener(@SmartCamera.Exception int exception) {
+        Log.e("LAG", exception+"");
     }
 }
---
+++ app/src/main/java/com/troshkova/portfolioprogect/visiondetector/Rotation.java
@@ -0,0 +1,21 @@
+package com.troshkova.portfolioprogect.visiondetector;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+@Retention(SOURCE)
+@IntDef({
+        Rotation.ROTATION_0,
+        Rotation.ROTATION_90,
+        Rotation.ROTATION_180,
+        Rotation.ROTATION_270
+})
+public @interface Rotation {
+    int ROTATION_0 =0;
+    int ROTATION_90 =90;
+    int ROTATION_180 =180;
+    int ROTATION_270 =270;
+}
